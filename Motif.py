ae5174950458cef3b9ba2c5848807a388adbcd32
Augustin Debeauquenne <augustin.debeauquenne@etu.univ-littoral.fr>
2026-06-09 15:54:07 +0200

    changement motif

--- Motif.py
+++ Motif.py
@@ -1,14 +1,16 @@
+from Case import *
+
 class Motif:
     """Un motif est une liste de cases ayant une taille (un nombre de cases)
     """
     
-    def __init__(self, liste_cases) -> None:
+    def __init__(self, liste_cases : list[Case]) -> None:
         self.taille : int = len(liste_cases)
-        self.liste_cases : list = liste_cases
+        self.liste_cases : list[Case] = liste_cases
         
     def getTaille(self) -> int:
         return(self.taille)
     
-    def getListeCases(self):
+    def getListeCases(self) -> list[Case]:
         return(self.liste_cases)
             
