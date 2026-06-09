4918c2997d6eba8c34d6363a9fca255fee268238
Abds07 <aurore.bds10@gmail.com>
2026-06-09 16:53:58 +0200

    peaufination des codes

--- Case.py
+++ Case.py
@@ -1,23 +1,26 @@
 class Case:
 
     """
-    Création de la classe case
+    Création de la classe case composé de ses coordonéées ligne, colonne et de sa valeur
     """
 
-    def __init__(self, position_ligne: int, position_colonne:int, valeur:int) -> None:
+    def __init__(self, position_ligne: int, position_colonne:int, valeur:int) -> None: # affectation des attributs de la classe Case
         self.position_ligne = position_ligne
         self.position_colonne = position_colonne
         self.valeur = valeur
 
-    def getPositionCase(self) -> tuple:
+    def getPositionCase(self) -> tuple: # renvoie un tuple composé de la position 
         return (self.position_colonne, self.position_ligne)
     
-    def setValeur(self, nouvelle_valeur) -> int:
-        self.valeur = nouvelle_valeur
+    def setValeur(self, nouvelle_valeur:int) -> None: # affectation d'une nouvelle valeur
+        if self.valeur != None:
+            print("vous ne pouvez pas modifier la valeur initiale") # vérification de l'existence d'une valeur sur une case
+        else:
+            self.valeur = nouvelle_valeur 
 
-    def setPositionLigne(self, nouvelle_position_ligne) -> None:
+    def setPositionLigne(self, nouvelle_position_ligne: int) -> None: # modification de la position de la ligne de la case
         self.position_ligne = nouvelle_position_ligne
 
-    def setPositionColonne(self, nouvelle_position_colonne) -> None:
+    def setPositionColonne(self, nouvelle_position_colonne:int) -> None: # modification de la position de la colonne de la case
         self.position_colonne = nouvelle_position_colonne
     
--- Grille.py
+++ Grille.py
@@ -1,10 +1,16 @@
 from Motif import *
 class Grille:
 
-    def __init__(self, taille, dictionnaire: dict):
+    """
+    Création de la classe Grille composé de sa taille un d'un 
+    dictionnaire pour lequel sa clé est le nom du motif et sa valeur 
+    est de la classe motif
+    """
+
+    def __init__(self, taille, dico: dict):
         self.taille = taille
-        self.dictionnaire = {}
+        self.dico = {}
     
 
-    def ajout_motif(self, motif: Motif, nom_motif: str):
+    def ajout_motif(self, motif: Motif, nom_motif: str) -> None: # implémentation des motifs à la grille
         self.dictionnaire[nom_motif] = motif
