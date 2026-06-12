710e8686ea5472a52e52e7089def4e97321cb15d
Abds07 <aurore.bds10@gmail.com>
2026-06-12 16:28:31 +0200

    rajout de fonction et correction

--- Grille.py
+++ Grille.py
@@ -87,3 +87,47 @@ class Grille:
             # on crée le motif avec ses cases et on l'ajoute à la grille
             nouveau_motif = Motif(cases_du_motif)
             self.ajout_motif(nouveau_motif)
+
+    def resoudre_backtracking(self) -> bool:
+        """
+        Résout la grille automatiquement.
+        Retourne True si une solution a été trouvée, False sinon.
+        """
+        # On cherche la première case vide 
+        case_vide = None
+        for case in self.dictionnaire_cases.values():
+            if case.valeur is None:
+                case_vide = case
+                break
+
+        # S'il n'y a plus de case vide, la grille est résolue 
+        if case_vide is None:
+            return True
+
+        l = case_vide.position_ligne
+        c = case_vide.position_colonne
+
+        # On trouve le motif de cette case pour connaître sa taille maximum N
+        motif_associe = None
+        for m in self.motifs:
+            if case_vide in m.liste_cases:
+                motif_associe = m
+                break
+
+        taille_max = motif_associe.getTaille()
+
+        # On teste toutes les valeurs possibles de 1 à N
+        for v in range(1, taille_max + 1):
+            case_vide.valeur = v  # On tente de poser le chiffre
+
+            # On vérifie si ce chiffre respecte les voisins et les contraintes du motif
+            if self.verifier_voisins(l, c) and motif_associe.estValide():
+                # Si c'est bon, on demande de résoudre la case vide suivante
+                if self.resoudre_backtracking():
+                    return True  # La solution s'est propagée jusqu'au bout !
+
+            # Si pas de solution possible pour l'instant on passe au chiffre suivant
+            case_vide.valeur = None
+
+        # Si aucun chiffre de 1 à N ne fonctionne, cette branche est une impasse
+        return False
